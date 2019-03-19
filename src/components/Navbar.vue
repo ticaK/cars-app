f24e3f9a7d07958d7232c2f4d306cb7e2b632f3e
Tijana Koprivica <tijanak1991@hotmail.com>
2019-03-19 18:28:29 +0100

    1

---
+++ src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+    <ul>
+
+    <li><strong><router-link to="/cars">Cars</router-link></strong></li>  
+   
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+li {
+  float: left;
+}
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+li a:hover {
+  background-color: #111;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
+import AppCars from './components/AppCars'
+
+
+
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+const routes = [
+  {path:'/',component: AppCars},
+  {path:'/cars',component: AppCars},
+  // {path:'/products',component: AppProducts},
+  // {path:'/customers/:id',component: AppCustomers, name:"latest-purchases"},
+  // {path:'/products/:id',component: AppProducts, name:"single-product"}
+  
+]
+
+const router = new VueRouter({
+  routes: routes,
+  mode:'history'
+  
+})
+
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
